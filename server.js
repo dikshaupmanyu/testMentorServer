8a51bc62730f995bad50fca585b87854a2b2ecea
Diksha Upmanyu <diksha.u@vsple.com>
2022-07-27 16:54:23 +0530

    add changes

--- server.js
+++ server.js
@@ -1329,9 +1329,9 @@ return res.redirect('/');
 
 });
 /////////////////////////////////////////
-var httpServer = http.createServer(app);
-// var httpsServer = https.createServer(credentials, app);
+// var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(port);
-// httpsServer.listen(port);
+// httpServer.listen(port);
+httpsServer.listen(port);
 console.log('The magic happens on port ' + port);
